dae56eec7f1520603e4189516a9f897e145fa8e8
Alex <odatfg@gmail.com>
2025-01-26 16:04:42 +0100

    Hecho por el boss

---
+++ src/Entity/Juego.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\JuegoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: JuegoRepository::class)]
+class Juego
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nombre = null;
+
+    #[ORM\Column]
+    private ?int $apuestaMinima = null;
+
+    #[ORM\Column]
+    private ?int $apuestaMaxima = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): static
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getApuestaMinima(): ?int
+    {
+        return $this->apuestaMinima;
+    }
+
+    public function setApuestaMinima(int $apuestaMinima): static
+    {
+        $this->apuestaMinima = $apuestaMinima;
+
+        return $this;
+    }
+
+    public function getApuestaMaxima(): ?int
+    {
+        return $this->apuestaMaxima;
+    }
+
+    public function setApuestaMaxima(int $apuestaMaxima): static
+    {
+        $this->apuestaMaxima = $apuestaMaxima;
+
+        return $this;
+    }
+}
---
+++ src/Repository/JuegoRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Juego;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Juego>
+ */
+class JuegoRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Juego::class);
+    }
+
+    //    /**
+    //     * @return Juego[] Returns an array of Juego objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('j')
+    //            ->andWhere('j.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('j.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Juego
+    //    {
+    //        return $this->createQueryBuilder('j')
+    //            ->andWhere('j.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+}
