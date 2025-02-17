4f8a66cb84df7d1054f8eb355f1f0a0c9a33f62d
Omar <omardzc94@gmail.com>
2025-02-17 12:01:04 +0100

    dashboard hecho

---
+++ LuckyLoop/BACKEND/src/Controller/Admin/DashboardController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Juego;
+use App\Entity\Partida;
+use App\Entity\PerfilEconomico;
+use App\Entity\Recompensa;
+use App\Entity\Usuario;
+use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        return $this->render('admin/dashboard.html.twig');
+        return parent::index();
+
+        // Option 1. You can make your dashboard redirect to some common page of your backend
+        //
+        // 1.1) If you have enabled the "pretty URLs" feature:
+        // return $this->redirectToRoute('admin_user_index');
+        //
+        // 1.2) Same example but using the "ugly URLs" that were used in previous EasyAdmin versions:
+        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+
+        // Option 2. You can make your dashboard redirect to different pages depending on the user
+        //
+        // if ('jane' === $this->getUser()->getUsername()) {
+        //     return $this->redirectToRoute('...');
+        // }
+
+        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        //
+        // return $this->render('some/path/my-dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('BACKEND');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Juego', 'fas fa-list', Juego::class);
+        yield MenuItem::linkToCrud('Partida', 'fas fa-list', Partida::class);
+        yield MenuItem::linkToCrud('Usuario', 'fas fa-list', Usuario::class);
+        yield MenuItem::linkToCrud('Perfil Económico', 'fas fa-list', PerfilEconomico::class);
+        yield MenuItem::linkToCrud('Recompensas', 'fas fa-list', Recompensa::class);
+    }
+}
---
+++ LuckyLoop/BACKEND/src/Controller/Admin/JuegoCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Juego;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class JuegoCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Juego::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
