54436ac01841d34b8e21f412220e4d6cff1548e7
Alex <alejandrojerez@MacBook-Pro-de-David-2.local>
2025-05-26 20:26:48 +0200

    .

--- LuckyLoop/FRONTEND/src/app/services/partida/partida.service.ts
+++ LuckyLoop/FRONTEND/src/app/services/partida/partida.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
     providedIn: 'root'
 })
 export class PartidaService {
-    private apiUrl = 'https://localhost:8000/api'
+    private apiUrl = 'http://localhost:8000/api'
 
     constructor(
         private http: HttpClient
